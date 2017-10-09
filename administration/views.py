0cdf9811fd3f6bf6c68c8f3df2bea2e93d850f35
julianesja <julianesja@gmail.com>
2017-10-09 09:24:06 -0500

    carga token de administrador

--- administration/views.py
+++ administration/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import json
 from Expa.ExpaToken import ExpaToken
+from lcperformance.models import user_register
 # Create your views here.
 
 @csrf_exempt
@@ -18,3 +19,16 @@ def getToken (request):
         else:
             resultado = {'resultado': True, "token": Token}
         return HttpResponse(json.dumps(resultado), content_type="application/json")
+
+@csrf_exempt
+def getTokenAdmin(request):
+    if request.method == "GET":
+        objUserRegister = user_register.objects.filter(user_expa='dev.colombia@ai.aiesec.org')[0]
+        objExpaToken = ExpaToken(objUserRegister.user_expa, objUserRegister.password_expa)
+        Token = objExpaToken.getToken()
+        if Token == None:
+            resultado = {'resultado': False}
+        else:
+            resultado = {'resultado': True, "token": Token}
+        return HttpResponse(json.dumps(resultado), content_type="application/json")
+
