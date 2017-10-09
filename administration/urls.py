e6496351d046b632b86521ffc88fd6402d44a6e3
julianesja <julianesja@gmail.com>
2017-10-09 09:37:58 -0500

    carga token de administrador

--- administration/urls.py
+++ administration/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from administration.views import getToken
+from administration.views import getToken, getTokenAdmin
 
 
 urlpatterns = [
     url(r'^token/$', getToken),
+    url(r'^tokenadmin/$', getTokenAdmin),
 ]
